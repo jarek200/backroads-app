ec35dcd30e6869b109d0911920e46afc65b86c85
Jarek Wyprzal <jwyprzal@gmail.com>
2023-05-30 19:29:02 +0100

    modify footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,46 +1,37 @@
 export default function Footer() {
+  const footerNav = [
+    { href: '#home', label: 'home' },
+    { href: '#about', label: 'about' },
+    { href: '#services', label: 'services' },
+    { href: '#featured', label: 'featured' },
+  ]
+
+  const footerIcons = [
+    { href: 'https://www.facebook.com', iconClass: 'fab fa-facebook', target: '_blank' },
+    { href: 'https://www.twitter.com', iconClass: 'fab fa-twitter', target: '_blank' },
+    { href: 'https://www.squarespace.com', iconClass: 'fab fa-squarespace', target: '_blank' },
+  ]
+
   return (
     <>
-      {' '}
       <footer className='section footer'>
         <ul className='footer-links'>
-          <li>
-            <a href='#home' className='footer-link'>
-              home
-            </a>
-          </li>
-          <li>
-            <a href='#about' className='footer-link'>
-              about
-            </a>
-          </li>
-          <li>
-            <a href='#services' className='footer-link'>
-              services
-            </a>
-          </li>
-          <li>
-            <a href='#featured' className='footer-link'>
-              featured
-            </a>
-          </li>
+          {footerNav.map((item, index) => (
+            <li key={index}>
+              <a href={item.href} className='footer-link'>
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
         <ul className='footer-icons'>
-          <li>
-            <a href='https://www.twitter.com' target='_blank' className='footer-icon'>
-              <i className='fab fa-facebook'></i>
-            </a>
-          </li>
-          <li>
-            <a href='https://www.twitter.com' target='_blank' className='footer-icon'>
-              <i className='fab fa-twitter'></i>
-            </a>
-          </li>
-          <li>
-            <a href='https://www.twitter.com' target='_blank' className='footer-icon'>
-              <i className='fab fa-squarespace'></i>
-            </a>
-          </li>
+          {footerIcons.map((item, index) => (
+            <li key={index}>
+              <a href={item.href} target={item.target} className='footer-icon'>
+                <i className={item.iconClass}></i>
+              </a>
+            </li>
+          ))}
         </ul>
         <p className='copyright'>
           copyright &copy; Backroads travel tours company
